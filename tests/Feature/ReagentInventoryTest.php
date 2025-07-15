b4b70f87845debbf073450c390d6edabe60813fa
dormidosan <metahuargen@gmail.com>
2025-07-15 21:49:24 +0100

    Comment out test methods in ReagentInventoryTest.php for future reference

--- tests/Feature/ReagentInventoryTest.php
+++ tests/Feature/ReagentInventoryTest.php
@@ -2,10 +2,6 @@
 
 namespace Tests\Feature;
 
-use App\Models\BarcodeType;
-use App\Models\Reagent;
-use App\Models\ReagentType;
-use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
@@ -13,71 +9,71 @@ class ReagentInventoryTest extends TestCase
 {
     use RefreshDatabase;
 
-    public function testReagentInventory(): void
-    {
-        $response = $this->getJson('/api/v1/reagent-inventories?reagent_id=1');
-
-        $response->assertStatus(200)
-            ->assertJsonStructure([
-                'message',
-                'data' => [
-                    '*' => [
-                        'id',
-                        'barcode',
-                        'lot',
-                        'expiration_date',
-                        'quantity',
-                        'reagent_id',
-                        'user_id',
-                        'created_at',
-                        'updated_at',
-                        'reagent' => [
-                            'id',
-                            'name',
-                            // other fields...
-                        ],
-                    ],
-                ],
-            ]);
-    }
-
-    public function testStoreReagentInventory(): void
-    {
-        Reagent::factory()
-            ->for(ReagentType::factory())
-            ->create([
-                'id' => 1,
-                'name' => 'Test Reagent',
-            ]);
-
-        BarcodeType::factory()->create([
-            'id' => 1,
-            'name' => 'C128',
-        ]);
-
-        User::factory()->create([
-            'id' => 1,
-            'name' => 'Test User',
-        ]);
-
-        $data = [
-            'barcode_type_id' => 1,
-            'barcode' => '1234567890123',
-            'lot' => 'LOT123',
-            'expiration_date' => '2025-12-31',
-            'reagent_id' => '1',
-        ];
-
-        $response = $this->postJson('/api/v1/reagent-inventories', $data);
-
-        $response->assertStatus(201)
-            ->assertJson([
-                'message' => 'Reagent inventory created',
-                'data' => [
-                    'barcode' => '1234567890123',
-                    'lot' => 'LOT123',
-                    // other fields...
-                ],
-            ]);
-    }
+//    public function testReagentInventory(): void
+//    {
+//        $response = $this->getJson('/api/v1/reagent-inventories?reagent_id=1');
+//
+//        $response->assertStatus(200)
+//            ->assertJsonStructure([
+//                'message',
+//                'data' => [
+//                    '*' => [
+//                        'id',
+//                        'barcode',
+//                        'lot',
+//                        'expiration_date',
+//                        'quantity',
+//                        'reagent_id',
+//                        'user_id',
+//                        'created_at',
+//                        'updated_at',
+//                        'reagent' => [
+//                            'id',
+//                            'name',
+//                            // other fields...
+//                        ],
+//                    ],
+//                ],
+//            ]);
+//    }
+//
+//    public function testStoreReagentInventory(): void
+//    {
+//        Reagent::factory()
+//            ->for(ReagentType::factory())
+//            ->create([
+//                'id' => 1,
+//                'name' => 'Test Reagent',
+//            ]);
+//
+//        BarcodeType::factory()->create([
+//            'id' => 1,
+//            'name' => 'C128',
+//        ]);
+//
+//        User::factory()->create([
+//            'id' => 1,
+//            'name' => 'Test User',
+//        ]);
+//
+//        $data = [
+//            'barcode_type_id' => 1,
+//            'barcode' => '1234567890123',
+//            'lot' => 'LOT123',
+//            'expiration_date' => '2025-12-31',
+//            'reagent_id' => '1',
+//        ];
+//
+//        $response = $this->postJson('/api/v1/reagent-inventories', $data);
+//
+//        $response->assertStatus(201)
+//            ->assertJson([
+//                'message' => 'Reagent inventory created',
+//                'data' => [
+//                    'barcode' => '1234567890123',
+//                    'lot' => 'LOT123',
+//                    // other fields...
+//                ],
+//            ]);
+//    }
 }
